da1c56eedeb3680fbce09c55be440705cb409e65
pkkdpr <oliwia.giercza@gmail.com>
2022-10-13 09:39:56 -0500

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+const canvas = document.querySelector("canvas");
+canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+const background = new Image();
+background.src = "../images/road.png";
+const car = new Image();
+car.src = "../images/car.png"
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(car, 200, 525, 100, 150);
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
